cd81787e7abb4aba22804643ee9c69816833d8d6
stacykimilu <45062735+stacykimilu@users.noreply.github.com>
2024-07-08 00:57:03 +0300

    Update nyc_api.py

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -1,14 +1,16 @@
-
 import requests
 import json
 
 class GetPrograms:
 
-  def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+    def get_programs(self):
+        URL = "https://data.cityofnewyork.us/resource/uvks-tn5n.json"
 
-    response = requests.get(URL)
-    return response.content
+        response = requests.get(URL)
+        return response.content
 
+# Create an instance of GetPrograms and call the get_programs() method
 programs = GetPrograms().get_programs()
+
+# Print the JSON response
 print(programs)
